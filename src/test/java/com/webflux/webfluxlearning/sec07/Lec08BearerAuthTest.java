31981ef095174e972ec0c85be7a90807af7c5d59
Ramjee Kushwaha <ashish23481@gmail.com>
2026-03-15 23:43:17 +0530

    section 07 completed - WebClient: Non-Blocking Microservices Communication

---
+++ src/test/java/com/webflux/webfluxlearning/sec07/Lec08BearerAuthTest.java
@@ -0,0 +1,43 @@
+package com.webflux.webfluxlearning.sec07;
+
+import com.webflux.webfluxlearning.sec07.dto.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
+
+/*
+ * Copyright (c) 2026 Ramjee Prasad
+ * Licensed under a custom Non-Commercial, Attribution, Share-Alike License.
+ * See the LICENSE file in the project root for full license information.
+ *
+ * Project: WebFluxLearning
+ * Package: com.webflux.webfluxlearning.sec07
+ * Created by: Ashish Kushwaha on 15-03-2026 22:52
+ * File: Lec07BasicAuthTest
+ *
+ * This source code is intended for educational and non-commercial purposes only.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   - Attribution must be given to the original author.
+ *   - The code must be shared under the same license.
+ *   - Commercial use is strictly prohibited.
+ *
+ */
+public class Lec07BasicAuthTest extends AbstractWebClient {
+    private final WebClient client = create(b -> b.defaultHeaders(h -> h.setBasicAuth("java", "secret")));
+
+    @Test
+    public void basicAuth() {
+
+        this.client.get()
+                .uri("/lec07/product/{id}", 1)
+                .retrieve()
+                .bodyToFlux(Product.class)
+                .doOnNext(print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+
+    }
+}
---
+++ src/test/java/com/webflux/webfluxlearning/sec07/Lec09ExchangeFilterTest.java
@@ -0,0 +1,22 @@
+package com.webflux.webfluxlearning.sec07;
+
+/*
+ * Copyright (c) 2026 Ramjee Prasad
+ * Licensed under a custom Non-Commercial, Attribution, Share-Alike License.
+ * See the LICENSE file in the project root for full license information.
+ *
+ * Project: WebFluxLearning
+ * Package: com.webflux.webfluxlearning.sec07
+ * Created by: Ashish Kushwaha on 15-03-2026 23:19
+ * File: ExchangeFilterTest
+ *
+ * This source code is intended for educational and non-commercial purposes only.
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   - Attribution must be given to the original author.
+ *   - The code must be shared under the same license.
+ *   - Commercial use is strictly prohibited.
+ *
+ */
+public class ExchangeFilterTest {
+}
